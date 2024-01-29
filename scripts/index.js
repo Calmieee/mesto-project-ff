445d0a2efe051d4db9806fc4e38e1ee5feb62b49
Rafael2lk <safiullinrafa@yandex.ru>
2024-01-29 19:25:25 +0300

    инициализация переменных для отображения карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,6 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('.card-template');
+const cardImage = cardTemplate.querySelector('.card__image');
+const cardTitle = cardTemplate.querySelector('card__title');
