c50cb14ab13f1c22646b3bae9879629e4822e02f
Rafael2lk <safiullinrafa@yandex.ru>
2024-01-30 01:00:53 +0300

    обработчик закрытия попапа добавления новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,19 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 const profileSection = document.querySelector('.profile.page__section');
-const addButton = profileSection.querySelector('.profile__add-button');
+const popupOpenButton = profileSection.querySelector('.profile__add-button');
+const popupNewCard = document.querySelector('.popup.popup_type_new-card');
+const popupClosedButton = popupNewCard.querySelector('.popup__close');
 
-addButton.addEventListener('click', function(){
-  const popupNewCard = document.querySelector('.popup.popup_type_new-card');
+
+popupOpenButton.addEventListener('click', function(){
   popupNewCard.classList.add('popup_is-opened');
-
 });
+
+popupClosedButton.addEventListener('click', function(){
+  popupNewCard.classList.remove('popup_is-opened');
+});
+
 
 //подумать над тем, можно ли так оставить или всё-таки написать другое решение
 initialCards.forEach(function(item){ 
