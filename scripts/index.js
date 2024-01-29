63e4478a39cfdeb736f78700be36a582cdb5e1bd
Rafael2lk <safiullinrafa@yandex.ru>
2024-01-29 20:44:14 +0300

    добавил функцию удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,17 @@
 // @todo: Вывести карточки на страницу
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
-
-initialCards.forEach(function(item){
+//подумать над тем, можно ли так оставить или всё-таки написать другое решение
+initialCards.forEach(function(item){ 
   const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
   cardElement.querySelector('.card__image').src = item.link;
   cardElement.querySelector('.card__title').textContent = item.name;
+
+  deleteButton.addEventListener('click', function(){
+    const listItem = deleteButton.closest('.places__item.card');
+    listItem.remove();
+  });
   return placesList.append(cardElement);
 });
-
